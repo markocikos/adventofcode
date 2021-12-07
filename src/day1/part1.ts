29d749fd3b34c9091a61eaecabf972a1a9af0da9
Marko Čikoš <marko.cikos@liferay.com>
2021-12-07 15:01:14 +0100

    day1 - prettier

--- src/day1/part1.ts
+++ src/day1/part1.ts
@@ -1,22 +1,22 @@
-import lineReader from "line-reader";
+import lineReader from 'line-reader';
 
 let increasesCount: number = 0;
 let lastDepth: number = Number.MAX_VALUE;
 
-lineReader.eachLine("inputs", function (line: string, last: string) {
-  const depth: number = parseInt(line, 10);
+lineReader.eachLine('inputs', function (line: string, last: string) {
+	const depth: number = parseInt(line, 10);
 
-  if (depth > lastDepth) {
-    increasesCount++;
+	if (depth > lastDepth) {
+		increasesCount++;
 
-    console.log(depth + " (increased) - count: " + increasesCount);
-  } else {
-    console.log(depth + " (decreased)");
-  }
+		console.log(depth + ' (increased) - count: ' + increasesCount);
+	} else {
+		console.log(depth + ' (decreased)');
+	}
 
-  lastDepth = depth;
+	lastDepth = depth;
 
-  if (last) {
-    console.log("Number of increases = " + increasesCount);
-  }
+	if (last) {
+		console.log('Number of increases = ' + increasesCount);
+	}
 });
--- src/day1/part2.ts
+++ src/day1/part2.ts
@@ -1,28 +1,29 @@
-import lineReader from "line-reader";
+import lineReader from 'line-reader';
 
 const inputs: Array<number> = [];
 
-lineReader.eachLine("inputs", function (line: string, last: string) {
-  inputs.push(parseInt(line, 10));
+lineReader.eachLine('inputs', function (line: string, last: string) {
+	inputs.push(parseInt(line, 10));
 
-  if (last) {
-    run(inputs);
-  }
+	if (last) {
+		run(inputs);
+	}
 });
 
 const run = (measurements: Array<number>) => {
-  let increasesCount = 0;
-  let lastWindow = Number.MAX_VALUE;
+	let increasesCount = 0;
+	let lastWindow = Number.MAX_VALUE;
 
-  for (let i = 0; i < measurements.length - 2; i++) {
-    const window = measurements[i] + measurements[i + 1] + measurements[i + 2];
+	for (let i = 0; i < measurements.length - 2; i++) {
+		const window =
+			measurements[i] + measurements[i + 1] + measurements[i + 2];
 
-    if (window > lastWindow) {
-      increasesCount++;
-    }
+		if (window > lastWindow) {
+			increasesCount++;
+		}
 
-    lastWindow = window;
-  }
+		lastWindow = window;
+	}
 
-  console.log("Number of increases = " + increasesCount);
+	console.log('Number of increases = ' + increasesCount);
 };
